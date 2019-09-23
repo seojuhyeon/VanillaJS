99eaa59a1c99ddb458bcf79b8546fb9a3bb78f5e
jokebear <wbgus@naver.com>
2019-09-23 16:57:37 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const title= document.getElementById("title");
+console.log(title);
+
+
+
+
+
+/*
+const monday="Mon";
+const tuesday="Tue";
+const wednesday ="Wed";
+const thursday="Thu";
+const friday="Fri";
+
+const nicoInfo = {name:"Nico",age:33,
+gender:"Male",isHandsome:true,
+favMovies:["Along the gods","Oldboy"],
+favFood:[
+   {
+      name:"Kimchi",
+      fatty:false
+    },
+    {
+      name:"Cheese Burger",
+      fatty:true
+    }
+  ]
+};
+//object : {}
+//array : []
+// const / let
+
+function sayHello(potato,chicken){
+  return `Hello ${potato} you are ${chicken} years old`;
+}
+const greetNicolas = sayHello("Nicolas",15);
+//console.log(greetNicolas);
+// function funname(parametername){}
+
+
+const calculator={
+  plus:function(a,b){
+    return a+b;
+   },
+  minus:function(a,b){
+    return a-b;
+  },
+  mul:function(a,b){
+    return a*b;
+  },
+  div:function(a,b){
+    return a/b;
+  }
+}
+
+const p = calculator.plus(5,5);
+const m = calculator.minus(5,5);
+console.log(p,m);
+*/
