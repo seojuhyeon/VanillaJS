a45e26015f6ac23070217f3e4231061fe2f76665
jokebear <wbgus@naver.com>
2019-09-23 21:22:40 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const title= document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-title.classList.toggle(CLICKED_CLASS);
+title.classList.toggle(CLICKED_CLASS);}
 /*const hasClass = title.classList.contains(CLICKED_CLASS);
 if(hasClass)
 title.classList.remove(CLICKED_CLASS);
 else {
 title.classList.add(CLICKED_CLASS);
 }*/
-}
+
 
 title.addEventListener("click",handleClick);
 
